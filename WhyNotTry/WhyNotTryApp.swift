0b3312fa15cf2cb8d4b45575555543a0b077a093
Paulo Baltieri <baltieri_paulo@hotmail.com>
2024-04-06 19:20:27 -0300

    Criando a interface gráfica

---
+++ WhyNotTry/WhyNotTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhyNotTryApp.swift
+//  WhyNotTry
+//
+//  Created by Paulo Baltieri on 06/04/24.
+//
+
+import SwiftUI
+
+@main
+struct WhyNotTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
