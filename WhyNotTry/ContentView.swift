086a1b542f1da43036b392f4ce1b4674923861f2
Paulo Baltieri <baltieri_paulo@hotmail.com>
2024-04-10 20:19:30 -0300

    adicionando novos icones

--- WhyNotTry/ContentView.swift
+++ WhyNotTry/ContentView.swift
@@ -9,13 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Circle()
+            .fill(.blue)
+            .padding()
+        overlay(
+            Image(systemName: "figure.soccer")
+        )
     }
 }
 
